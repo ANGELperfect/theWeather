0456f33f229edf4c949a2c5e5e1c7ea023eebc79
Your Name <you@example.com>
2020-09-15 20:55:55 +0100

    Updates

---
+++ precache-manifest.dac8cd48bc9293c28bb90a91bc1b869e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "669b85abdd5b225f147e",
+    "url": "/theWeather/static/css/main.322b0ca7.chunk.css"
+  },
+  {
+    "revision": "669b85abdd5b225f147e",
+    "url": "/theWeather/static/js/main.020438da.chunk.js"
+  },
+  {
+    "revision": "822a3902ae3854366e31",
+    "url": "/theWeather/static/js/runtime~main.4d76e0e3.js"
+  },
+  {
+    "revision": "b33bef510142b9ce851f",
+    "url": "/theWeather/static/js/2.e3d5bc46.chunk.js"
+  },
+  {
+    "revision": "b966e723db080e85e86b0e7886043e5e",
+    "url": "/theWeather/index.html"
+  }
+];
